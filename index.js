8dc4eda9afa8245231e20a4d015dc874a057feac
malkiAbdelhamid <a.malki@esi-sba.dz>
2024-10-22 23:28:39 +0100

    first

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from 'express';
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
 let books = [
-  { id: 1, title: 'The Great Gatssssby', author: 'F. Scott Fitzgerald' },
+  { id: 1, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
   { id: 2, title: '1984', author: 'George Orwell' }
   ,{ id: 3, title: 'To Kill a Mockingbird', author: 'Harper Lee' }
 ];
